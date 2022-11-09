0255630c07b7f66423b1ad2c3dff1310d1cde418
devsecops101 <62809843+devsecops101@users.noreply.github.com>
2022-11-09 22:27:55 +0800

    Delete login.php

--- install_files/login.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head><title>Simple Sign Up</title></head>
-<body>
-<form action="cuser.php" method="post">
-Username: <input type="text" name="uname"><br>
-Password: <input type="password" name="pass"><br>
-<input type="submit" value="Submit Account">
-</body>
-</html> 
